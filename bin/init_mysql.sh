b4c5d369f38f3cc0ba87e06ab219c520004db274
miramir <gmiramir@gmail.com>
2015-07-03 23:41:07 +0500

    init mysql

--- bin/init_mysql.sh
+++ bin/init_mysql.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-sed -i 's/^\(bind-address\s.*\)/# \1/' /etc/mysql/my.cnf && \
-/usr/bin/mysqld_safe & 
+sed -i 's/^\(bind-address\s.*\)/# \1/' /etc/mysql/my.cnf
+/usr/bin/mysqld_safe &
 sleep 10s
 mysqladmin -u root password 'root'
 mysql -e 'GRANT ALL PRIVILEGES ON *.* TO \"root\"@\"%\" WITH GRANT OPTION;'
 killall mysqld
-sleep 10s
--- bin/init_php.sh
+++ bin/init_php.sh
@@ -2,7 +2,7 @@
 sed -i -e "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g" /etc/php5/fpm/php.ini && \
 sed -i "s/^;date\.timezone\s*=\s*$/date.timezone = Asia\/Yekaterinburg/g" /etc/php5/fpm/php.ini && \
 sed -i "s/^;mbstring\.internal_encoding\s*=.*$/mbstring\.internal_encoding = UTF-8/g" /etc/php5/fpm/php.ini && \
-sed -i "s/^daemonize\s*=\s*yes/daemonize = no/g" /etc/php5/fpm/php-fpm.conf && \
+sed -i "s/^;daemonize\s*=\s*yes/daemonize = no/g" /etc/php5/fpm/php-fpm.conf && \
 sed -i -e "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g" /etc/php5/cli/php.ini && \
 sed -i "s/^;date\.timezone\s*=\s*$/date.timezone = Asia\/Yekaterinburg/g" /etc/php5/cli/php.ini && \
 sed -i "s/^;mbstring\.internal_encoding\s*=.*$/mbstring\.internal_encoding = UTF-8/g" /etc/php5/cli/php.ini && \
